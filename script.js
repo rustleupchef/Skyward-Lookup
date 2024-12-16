143793b06466e7e2aaa276945841c4148d4a7da6
rustleupchef <rustle.up.chef@gmail.com>
2024-12-15 21:06:32 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function change() {
+    console.log(document.getElementById("Id").value)
+    const id = document.getElementById("Id").value;
+    const img = new Image();
+    const url = "https://skyward.springbranchisd.com/pictures/" + id + ".jpg";
+    img.src = url;
+    img.onload = function() {
+        document.getElementById("Person").src = url;
+    }
+    img.onerror = function() {
+        document.getElementById("Person").src = "default.png";
+    }
+}
