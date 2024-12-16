bdfcf980a92a697bbbd2e79eb5052b9c439f0ac8
arch_rival <123332908+rustleupchef@users.noreply.github.com>
2024-12-16 10:42:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let lastvalidimg = "default.png";
+
+window.onload = function() {
+    const id = document.getElementById("Id").value.substring(0, 6);
+    document.getElementById("Id").value = id;
+    const img = new Image();
+    const url = "https://skyward.springbranchisd.com/pictures/" + id + ".jpg";
+    img.src = url;
+    img.onload = function() {
+        document.getElementById("Person").src = url;
+        lastvalidimg = url;
+    }
+    img.onerror = function() {
+        document.getElementById("Person").src = lastvalidimg;
+    }
+}
+
 function change() {
-    console.log(document.getElementById("Id").value)
     const id = document.getElementById("Id").value.substring(0, 6);
     document.getElementById("Id").value = id;
     const img = new Image();
